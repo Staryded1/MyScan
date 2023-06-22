69131d862cfa8973e79f3ae74264c0bd478e000d
sasha <sasha.anisimov.90@bk.ru>
2023-06-22 16:42:21 +0700

    dfgs

--- app/src/main/java/com/example/myscan/MainActivity.java
+++ app/src/main/java/com/example/myscan/MainActivity.java
@@ -5,8 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,6 +26,22 @@ public class MainActivity extends AppCompatActivity {
 
         joinButton = (Button) findViewById(R.id.register_btn);
         loginButton = (Button)  findViewById(R.id.login_btn);
+
+        FirebaseApp.initializeApp(this);
+
+        // Получение ссылки на раздел "devices" и создание его в базе данных
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference devicesRef = database.getReference("devices");
+
+        devicesRef.setValue(null)
+                .addOnSuccessListener(aVoid -> {
+                    // Успешное создание раздела "devices" в базе данных
+                    Log.d("ScanRezult", "Раздел 'devices' успешно создан в базе данных");
+                })
+                .addOnFailureListener(e -> {
+                    // Ошибка при создании раздела "devices" в базе данных
+                    Log.e("ScanRezult", "Ошибка при создании раздела 'devices': " + e.getMessage());
+                });
 
 
 
