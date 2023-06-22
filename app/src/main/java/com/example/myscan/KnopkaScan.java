fdc1e4bad1488e6d31946e4ffc77a397d96bde8e
sasha <sasha.anisimov.90@bk.ru>
2023-06-22 19:36:53 +0700

    gdfgdg

--- app/src/main/java/com/example/myscan/KnopkaScan.java
+++ app/src/main/java/com/example/myscan/KnopkaScan.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 
@@ -20,39 +21,25 @@ public class KnopkaScan extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_knopka_scan);
         btn_scan = findViewById(R.id.btn_scan);
-        btn_scan.setOnClickListener(v ->
-        {
-            scanCode();
-        });
+        btn_scan.setOnClickListener(v -> scanCode());
     }
 
     private void scanCode() {
-
         ScanOptions options = new ScanOptions();
         options.setPrompt("Идет сканирование");
         options.setBeepEnabled(true);
         options.setOrientationLocked(true);
         options.setCaptureActivity(CaptureAct.class);
         barLaucher.launch(options);
-
     }
 
-    ActivityResultLauncher<ScanOptions> barLaucher = registerForActivityResult(new ScanContract(), result ->
-    {
-        if(result.getContents() !=null)
-        {
-            AlertDialog.Builder builder = new AlertDialog.Builder(KnopkaScan.this);
-            builder.setTitle("Результат сканирования");
-            builder.setMessage(result.getContents());
-            builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialog, int which)
-                {
-                    dialog.dismiss();
-
-                }
-            }).show();
+    ActivityResultLauncher<ScanOptions> barLaucher = registerForActivityResult(new ScanContract(), result -> {
+        if (result.getContents() != null) {
+            String scanResult = result.getContents();
+            // Переход на активити ScanRezult и передача результата сканирования
+            Intent intent = new Intent(KnopkaScan.this, ScanRezult.class);
+            intent.putExtra("scan_result", scanResult);
+            startActivity(intent);
         }
-
     });
 }
